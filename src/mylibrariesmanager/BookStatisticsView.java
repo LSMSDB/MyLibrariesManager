ae81f2abbaa4be80572c63a81cef39c56aaf8508
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2019-10-11 20:22:22 +0200

    Update BookStatisticsView.java

--- src/mylibrariesmanager/BookStatisticsView.java
+++ src/mylibrariesmanager/BookStatisticsView.java
@@ -10,27 +10,12 @@ import javafx.scene.chart.NumberAxis;
 
 public class BookStatisticsView extends BarChart<String, Number> {
 	private static ObservableList<XYChart.Data<String,Number>> observableStatisticsList;
-	
-	public BookStatisticsView(CategoryAxis xAxis, NumberAxis yAxis, List<BookStatistic> bookStatistics){
-		super(xAxis, yAxis);
-		for(int i=0 ; i < bookStatistics.size() ; i++) {
-			observableStatisticsList.add(new XYChart.Data<>(bookStatistics.get(i).getName(),bookStatistics.get(i).getNumberOfBorrowings()));
-		}
-	}
-	
-	public BookStatisticsView(CategoryAxis xAxis, NumberAxis yAxis){
-		super(xAxis, yAxis);
-	}
-	
-	public BookStatisticsView(List<BookStatistic> bookStatistics) {
-		super(new CategoryAxis(), new NumberAxis());
-		for(int i=0 ; i < bookStatistics.size() ; i++) {
-			observableStatisticsList.add(new XYChart.Data<>(bookStatistics.get(i).getName(),bookStatistics.get(i).getNumberOfBorrowings()));
-		}
-	}
-	
+
 	public BookStatisticsView() {
 		super(new CategoryAxis(), new NumberAxis());
+		this.getXAxis().setLabel("Books");
+		this.getXAxis().setLabel("Number of Borrowings");
+		this.setTitle("Most borrowed books");
 	}
 	
 	public void updateStatisticsList(List<BookStatistic> bookStatistics) {
