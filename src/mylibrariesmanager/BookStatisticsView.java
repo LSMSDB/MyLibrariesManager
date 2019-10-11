f6ff62bb1b48bcea9f6d3689eb5cbbe424aca9e5
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2019-10-11 19:44:07 +0200

    Update BookStatisticsView.java

--- src/mylibrariesmanager/BookStatisticsView.java
+++ src/mylibrariesmanager/BookStatisticsView.java
@@ -1,5 +1,42 @@
 package mylibrariesmanager;
 
-public class BookStatisticsView {
+import java.util.List;
 
+import javafx.collections.ObservableList;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
+public class BookStatisticsView extends BarChart<String, Number> {
+	private static ObservableList<XYChart.Data<String,Number>> observableStatisticsList;
+	
+	public BookStatisticsView(CategoryAxis xAxis, NumberAxis yAxis, List<BookStatistic> bookStatistics){
+		super(xAxis, yAxis);
+		for(int i=0 ; i < bookStatistics.size() ; i++) {
+			observableStatisticsList.add(new XYChart.Data<>(bookStatistics.get(i).getName(),bookStatistics.get(i).getNumberOfBorrowings()));
+		}
+	}
+	
+	public BookStatisticsView(CategoryAxis xAxis, NumberAxis yAxis){
+		super(xAxis, yAxis);
+	}
+	
+	public BookStatisticsView(List<BookStatistic> bookStatistics) {
+		super(new CategoryAxis(), new NumberAxis());
+		for(int i=0 ; i < bookStatistics.size() ; i++) {
+			observableStatisticsList.add(new XYChart.Data<>(bookStatistics.get(i).getName(),bookStatistics.get(i).getNumberOfBorrowings()));
+		}
+	}
+	
+	public BookStatisticsView() {
+		super(new CategoryAxis(), new NumberAxis());
+	}
+	
+	public void updateStatisticsList(List<BookStatistic> bookStatistics) {
+		observableStatisticsList.clear();
+		for(int i=0 ; i < bookStatistics.size() ; i++) {
+			observableStatisticsList.add(new XYChart.Data<>(bookStatistics.get(i).getName(),bookStatistics.get(i).getNumberOfBorrowings()));
+		}
+	}
 }
