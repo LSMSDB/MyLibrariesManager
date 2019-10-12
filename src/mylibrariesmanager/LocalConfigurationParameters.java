a6717e11539b3de9dfbdd8cb10f08f3755ba8909
mariegiannoni <52198581+mariegiannoni@users.noreply.github.com>
2019-10-12 16:48:02 +0200

    Update LocalConfigurationParameters.java

--- src/mylibrariesmanager/LocalConfigurationParameters.java
+++ src/mylibrariesmanager/LocalConfigurationParameters.java
@@ -1,5 +1,69 @@
 package mylibrariesmanager;
 
-public class LocalConfigurationParameters {
+import java.io.File;
+import java.io.IOException;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.*;
+import javax.xml.validation.*;
+import javax.xml.transform.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class LocalConfigurationParameters {
+	
+	private static String addressDBMS;
+	private static String portDBMS;
+	
+	public static void retrieveLocalConfiguration() {
+		Document document;
+		Schema schema;
+		String configurationFileXML = "configuration.xml";
+		String configurationFileXSD = "configuration.xsd";
+		
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			
+			document = builder.parse(new File(configurationFileXML));
+			schema = schemaFactory.newSchema(new File(configurationFileXSD));
+			
+			schema.newValidator().validate(new DOMSource(document));
+		} catch (ParserConfigurationException e) {
+	         e.printStackTrace();
+	         document = null;
+	    } catch (SAXException e) {
+	         e.printStackTrace();
+	         document = null;
+	    } catch (IOException e) {
+	         e.printStackTrace();
+	         document = null;
+	    }
+		
+		if (document == null)
+			System.exit(1);
+		
+		setAddressDBMS(document.getElementsByTagName("addressDBMS").item(0).getTextContent());
+		setPortDBMS(document.getElementsByTagName("portDBMS").item(0).getTextContent());
+	}
+
+	public static String getAddressDBMS() {
+		return addressDBMS;
+	}
+
+	public static void setAddressDBMS(String addressDBMS) {
+		LocalConfigurationParameters.addressDBMS = addressDBMS;
+	}
+
+	public static String getPortDBMS() {
+		return portDBMS;
+	}
+
+	public static void setPortDBMS(String portDBMS) {
+		LocalConfigurationParameters.portDBMS = portDBMS;
+	}
 }
